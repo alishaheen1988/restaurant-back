52aef46f712c0430f353c8fdd357fdc8d25a2d82
Ali <ali.shaheen001@tradinos.com>
2023-12-28 13:39:11 +0300

    add services

---
+++ app/Services/ItemService.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Services;
+
+use App\Models\Category;
+use App\Models\Item;
+use Illuminate\Validation\UnauthorizedException;
+use Symfony\Component\HttpFoundation\Exception\BadRequestException;
+
+class ItemService
+{
+    public function addItem($name, $price, $category_id, $desc=null, $discount_percentage=null)
+    {
+        $category=Category::find($category_id);
+        if ($category->user_id != \Auth::id())
+            throw new UnauthorizedException('UnAuthorized',403);
+        if ($category->children()->exists())
+            throw new BadRequestException('This category has subcategories');
+        return Item::create([
+            'name'=>$name,
+            'description'=>$desc,
+            'price'=>$price,
+            'category_id'=>$category_id,
+            'discount_percentage'=>$discount_percentage
+        ]);
+    }
+
+    public function updateItem($id,$updatedData)
+    {
+        $item=Item::find($id);
+        if ($item->category->user_id != \Auth::id())
+            throw new UnauthorizedException('UnAuthorized',403);
+       return $item->update($updatedData);
+    }
+
+    public function deleteItem($id)
+    {
+        $item=Item::find($id);
+        if ($item->category->user_id != \Auth::id())
+            throw new UnauthorizedException('UnAuthorized',403);
+       return $item->delete();
+    }
+}
