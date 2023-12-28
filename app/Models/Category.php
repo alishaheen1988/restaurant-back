3c25ef3b05437255c4dd5d3c3117ae325c6e2d3c
Ali <ali.shaheen001@tradinos.com>
2023-12-28 12:49:02 +0300

    add models and migrations

---
+++ app/Models/Category.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Category extends Model
+{
+    protected $with=['items'];
+    protected $guarded = [];
+    
+    public function parent()
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    public function children()
+    {
+        return $this->hasMany(Category::class, 'parent_id');
+    }
+
+    public function items()
+    {
+        return $this->hasMany(Item::class);
+    }
+}
